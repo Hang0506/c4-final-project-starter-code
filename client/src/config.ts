525fffd15c85b0c78bc7b3ccef32058707105de3
FSO-HANGNT60\User <ngohang.developers@gmail.com>
2022-06-24 11:33:50 +0700

    client

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'j3ammi0b33'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+// export const apiEndpoint = 'http://localhost:3000/dev'
+
+export const authConfig = {
+  domain: 'dev-gziuvt00.us.auth0.com',            // Auth0 domain
+  clientId: '8n77MQVLosN7nY3aTdLNZyFS7lODh1ET',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
