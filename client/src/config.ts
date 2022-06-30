75e1a3c059ebbac0e4e8ec390b03322e395a51be
FSO-HANGNT60\User <ngohang.developers@gmail.com>
2022-06-30 20:55:16 +0700

    finish-project

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-const apiId = '2ulx76qyfg'
+const apiId = '5wdrnp09e6'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 // export const apiEndpoint = 'http://localhost:3000/dev'
 
